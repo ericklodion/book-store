543cf7bd910d9cc10c47a5ce5b54e61d884fd9de
Erick Espinosa <erick.lodion@gmail.com>
2024-07-28 21:32:02 -0300

    Added migration with price on price table entity

---
+++ api/bs-api/bs-domain/Migrations/20240729002635_AddPriceToPriceTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bs_domain.Migrations
+{
+    public partial class AddPriceToPriceTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "BookPriceTable",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "BookPriceTable");
+        }
+    }
+}
